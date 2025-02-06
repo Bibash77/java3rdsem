7edac37c2784997de255181a3488d9ae346a9f49
Bibash7 <biswash166@gmail.com>
2025-02-06 22:36:47 +0545

    day1 code

---
+++ src/main/java/org/example/day1/Callable.java
@@ -0,0 +1,29 @@
+package org.example.day1;
+
+/**
+ * @author Bibash Bogati
+ * @created 2025-01-31
+ */
+public class Callable {
+
+
+    public static void main(String[] args) {
+        MyDetail myDetail = new MyDetail();
+        myDetail.id=1;
+        myDetail.name="Bibash";
+        myDetail.average = 60.0f;
+
+        MyDetail anotherDetail = new MyDetail();
+        myDetail.id=2;
+        myDetail.name="Ram";
+        myDetail.average = 70.0f;
+
+
+        if(myDetail.average > anotherDetail.average){
+            System.out.println("myDetail Average is greater than anotherDetail.average");
+        } else {
+            System.out.println("anotherDetail Average is greater than my.average");
+        }
+
+    }
+}
---
+++ src/main/java/org/example/day1/MyDetail.java
@@ -0,0 +1,12 @@
+package org.example.day1;
+
+/**
+ * @author Bibash Bogati
+ * @created 2025-01-31
+ */
+public class MyDetail {
+    int id = 1;
+    String name = "Bibash";
+    int marks = 100;
+    float average;
+}
