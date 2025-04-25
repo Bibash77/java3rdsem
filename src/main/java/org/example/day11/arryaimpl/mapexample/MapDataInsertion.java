309e936e0c28634e026e631902f1fde44cfb05fa
Bibash7 <biswash166@gmail.com>
2025-04-25 08:50:00 +0545

    map remaing

--- src/main/java/org/example/day11/arryaimpl/mapexample/MapDataInsertion.java
+++ src/main/java/org/example/day11/arryaimpl/mapexample/MapDataInsertion.java
@@ -13,23 +13,28 @@ public class MapDataInsertion {
 
     public static void main(String[] args) {
         Map<String, Integer> map = new HashMap();
+        // add element to map
         map.put("A", 1);
         map.put("B", 2);
         map.put("C", 3);
         map.put("C", 7);
         map.put("D", 4);
 
-
+// remove element
         map.remove("B");
 
-
+// get element and compare
         if(map.get("B") == 2){
             System.out.println("Yes B key is 2");
         }
 
-
+// loop based on key and value
         for (Map.Entry<String, Integer> individualValue : map.entrySet()) {
             System.out.println(individualValue.getKey() + " : " + individualValue.getValue());
+        }
+// loop key and get value from map
+        for (String key : map.keySet()) {  // map.values()
+            System.out.println(map.get(key));
         }
 
 //        Set<Map.Entry<String, Integer>> keySet = map.entrySet();
